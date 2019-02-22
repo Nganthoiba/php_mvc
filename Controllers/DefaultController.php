63dab943daa2f7350b2a083cf5202f63a5614555
Nganthoiba <Nganthoiba@DESKTOP-CLD3BN8>
2019-02-22 23:36:18 +0530

    Default Controller

---
+++ Controllers/DefaultController.php
@@ -0,0 +1,19 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * This is the default controller
+ *
+ * @author Nganthoiba
+ */
+
+class DefaultController extends Controller{
+    public function index(){
+        echo 'Hello this is the index action of default controller.';
+    }
+}
